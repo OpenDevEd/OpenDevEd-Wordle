ed0420c6ad55f58675e7098e110da1797f932b25
Merouane Grissen <mgrissen9@gmail.com>
2024-08-08 03:41:33 +0100

    update css

--- src/App.js
+++ src/App.js
@@ -92,31 +92,34 @@ function App() {
   useEffect(() => {
     fetchNewWord();
   }, []);
-
   return (
     <div className="game-container">
-      <h1>Wordle Clone</h1>
-      <div className="board">
-        {guesses.map((guess, i) => {
-          const isCurrentGuess = i === guesses.findIndex(val => val == null);
-          return (
-            <Line 
-              key={i}
-              guess={isCurrentGuess ? currentGuess : guess ?? ''}
-              isFinal={!isCurrentGuess && guess != null}
-              solution={solution}
-            />
-          );
-        })}
+      <header>
+        <h1>Wordle Clone</h1>
+      </header>
+      <div className="board-container">
+        <div className="board">
+          {guesses.map((guess, i) => {
+            const isCurrentGuess = i === guesses.findIndex(val => val == null);
+            return (
+              <Line 
+                key={i}
+                guess={isCurrentGuess ? currentGuess : guess ?? ''}
+                isFinal={!isCurrentGuess && guess != null}
+                solution={solution}
+              />
+            );
+          })}
+        </div>
+        <Keyboard onKeyPress={handleKeyPress} usedLetters={usedLetters} />
+        {isGameOver && (
+          <EndGamePopup 
+            isWin={isWin}
+            solution={solution}
+            onPlayAgain={resetGame}
+          />
+        )}
       </div>
-      <Keyboard onKeyPress={handleKeyPress} usedLetters={usedLetters} />
-      {isGameOver && (
-        <EndGamePopup 
-          isWin={isWin}
-          solution={solution}
-          onPlayAgain={resetGame}
-        />
-      )}
     </div>
   );
 }
