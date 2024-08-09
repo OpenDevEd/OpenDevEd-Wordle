5c1038e9a052e9d1592012748b89ee735e8d572a
HxX2 <zaka.hanix@gmail.com>
2024-08-09 11:39:43 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <div className="text-[100px] text-black flex items-center font-bold justify-center w-screen h-full ">
+        WORDLE GAME
+      </div>
+    </>
+  );
+}
+
+export default App;
