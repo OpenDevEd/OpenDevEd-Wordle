208cce6bcc35fbde2ee49c3978504d2165729a5e
Sarah Meftah <hichammeftah4@gmail.com>
2024-08-10 17:42:25 +0100

    Improved GuessBox animations

--- src/components/GuessBox.tsx
+++ src/components/GuessBox.tsx
@@ -6,8 +6,15 @@ export default function GuessBox({correctLetters}:{correctLetters: string[]}) {
         <motion.div className='flex gap-2 items-center justify-between'>
         {
             correctLetters.map((letter, index) => (
-                <motion.div key={index} className='text-white font-bold text-5xl w-[4.5rem] h-[4.5rem] flex items-center justify-center uppercase'
-                    style={{backgroundColor: letter.length === 0 ? "transparent" : "var(--green)" }}>
+                <motion.div 
+                    transition={{ duration: 0.5 }}
+                    key={index} className='text-white font-bold text-5xl w-[4.5rem] h-[4.5rem] flex items-center justify-center uppercase transition-all'
+                    style={
+                        {
+                            backgroundColor: letter.length === 0 ? "transparent" : "var(--green)" ,
+                            height: letter.length === 0 ? "0" : "",
+                        }
+                    }>
                     {letter}
                 </motion.div>
             ))
