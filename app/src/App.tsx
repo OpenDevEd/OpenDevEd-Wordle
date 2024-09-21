3433231f61b44025c907dc841c453cfb64f618ff
sekak <ahmadesekak@gmail.com>
2024-09-21 12:16:20 +0100

    Setup project

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GameBoard from './components/GameBoard';
+
+function App() {
+  return (
+    <div className="App text-red-500 text-[100px]">
+      <GameBoard/>
+    </div>
+  );
+}
+
+export default App;
